3a155e4b92e78ac35a0f1d95118007c31e84f9cb
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-07-26 15:00:04 +0500

    new loaders

--- src/components/loaders/Loader.js
+++ src/components/loaders/Loader.js
@@ -3,9 +3,9 @@ import { CircleNotch } from "@phosphor-icons/react";
 
 const Loader = () => {
     return (
-        <div className="loader_container">
+        <>
             <CircleNotch weight="bold" className="icon_min loader" />
-        </div>
+        </>
     );
 }
 
