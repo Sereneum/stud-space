055b75f56f7b1d4350a1e271af5dab9c27712d17
Sereneum <maks.rom.111@mail.ru>
2023-07-26 17:13:29 +0300

    fix themes

--- src/hooks/useThemeDetector.js
+++ src/hooks/useThemeDetector.js
@@ -5,11 +5,16 @@ import {themeManager} from "../managers/themeManager";
 const useThemeDetector = () => {
     const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)')
     const {localConfig} = useContext(Context)
+    const [isThemeDetector, setIsThemeDetector] = useState(null)
 
     darkModeQuery.addEventListener('change', () => {
-        if(localConfig.theme === 'system')
+        if(localConfig.theme === 'system') {
             themeManager(localConfig).setStyle()
+            setIsThemeDetector(themeManager(localConfig).isDark())
+        }
     })
+
+    return {isThemeDetector}
 }
 
 export default useThemeDetector
