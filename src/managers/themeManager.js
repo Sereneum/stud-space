97ebf0a69dc2eee2babef96cf9084fab607c3b75
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-08-01 14:49:52 +0300

    web app test

--- src/managers/themeManager.js
+++ src/managers/themeManager.js
@@ -12,7 +12,7 @@ const isDark = theme => {
     return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 }
 
-const statusBarStyle = isDark ? '#ededed' : 'black';
+const statusBarStyle = isDark ? 'black-translucent' : 'black';
 
 export const themeManager = localConfig => {
     return {
