cae66667a05d550b71ee523586f4df30b7de387f
Sereneum <maks.rom.111@mail.ru>
2023-08-06 21:59:09 +0300

    faq

--- src/managers/deadlinesManager.js
+++ src/managers/deadlinesManager.js
@@ -3,16 +3,16 @@ import {checkDeadline} from "./timeManager";
 // смешиваем
 const mixer = courses => {
     const tasks = []
-    for(let i of courses)
-        for(let j of i.tasks)
+    for (let i of courses)
+        for (let j of i.tasks)
             tasks.push(j)
     return tasks
 }
 // отбираем по времени
 const selectionByTime = t => {
     const r = []
-    for(let i of t)
-        if(checkDeadline(i.periodRealization) > 0)
+    for (let i of t)
+        if (checkDeadline(i.periodRealization) > 0)
             r.push(i)
     return r
 }
@@ -20,18 +20,19 @@ const selectionByTime = t => {
 const selectionByStatus = t => {
     let r = []
     // status = 2 -> в проверке, status = 0 -> не отправлено
-    for(let i of t)
-        if(i.statusID === 0 || i.statusID === 2)
+    for (let i of t)
+        if (i.statusID === 0 || i.statusID === 2)
             r.push(i)
     return r
 }
 
+
 // сортируем и обрезаем
-const conv = t =>
+const conv = (t, isSmallDevice) =>
     t
         .sort((a, b) => b.periodRealization - a.periodRealization)
-        .slice(-3)
+        .slice(isSmallDevice ? 0 : -3)
         .sort((a, b) => a.periodRealization - b.periodRealization)
 
-export const deadlinesManager = courses =>
-    conv(selectionByStatus(selectionByTime(mixer(courses))))
+export const deadlinesManager = (courses, isSmallDevice) =>
+    conv(selectionByStatus(selectionByTime(mixer(courses))), isSmallDevice)
