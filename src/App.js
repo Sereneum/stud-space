4567bf6380435ff629bb951342629504d77b7c67
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-08-11 22:22:11 +0300

    finally adaptive theme

--- src/App.js
+++ src/App.js
@@ -1,107 +1,102 @@
-import "./styles/main.css";
-import "./styles/schedule.css";
+import './styles/main.css'
+import './styles/schedule.css'
 
-import {Routes, Route, useLocation, Navigate} from "react-router-dom";
-import {useContext} from "react";
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { useContext } from 'react'
 
-import ScrollToTop from "./components/scroll_to_top/ScrollToTop";
+import ScrollToTop from './components/scroll_to_top/ScrollToTop'
 
-import Sky from "./components/sky/Sky";
-import Header from "./components/header/Header.js"
-import CoursesTablet from "./components/courses_block/CoursesTablet";
-import DeadlineTablet from "./components/deadline_block/DeadlineTablet";
-import Enter from "./components/enter_block/Enter";
-import Courses from "./components/courses_block/Courses";
-import Deadline from "./components/deadline_block/Deadline";
-import Schedule from "./components/schedule_block/Schedule";
-import Menu from "./components/menu_block/Menu";
-import Course from "./components/course_block/Course";
-import Task from "./components/task_block/Task";
-import Settings from "./components/settings_block/Settings";
-import PrivacyLogged from "./components/privacy_block/PrivacyLogged";
-import FaqLogged from "./components/faq_block/FaqLogged";
-import {Context} from "./index";
-import useApp from "./hooks/useApp";
-import PrivacyUnAuth from "./components/privacy_block/PrivacyUnAuth";
-import FaqUnAuth from "./components/faq_block/FaqUnAuth";
-import {observer} from "mobx-react-lite"
-import Loader from "./components/loaders/Loader";
-import {themeManager} from "./managers/themeManager";
-import useThemeDetector from "./hooks/useThemeDetector";
+import Sky from './components/sky/Sky'
+import Header from './components/header/Header.js'
+import CoursesTablet from './components/courses_block/CoursesTablet'
+import DeadlineTablet from './components/deadline_block/DeadlineTablet'
+import Enter from './components/enter_block/Enter'
+import Courses from './components/courses_block/Courses'
+import Deadline from './components/deadline_block/Deadline'
+import Schedule from './components/schedule_block/Schedule'
+import Menu from './components/menu_block/Menu'
+import Course from './components/course_block/Course'
+import Task from './components/task_block/Task'
+import Settings from './components/settings_block/Settings'
+import PrivacyLogged from './components/privacy_block/PrivacyLogged'
+import FaqLogged from './components/faq_block/FaqLogged'
+import { Context } from './index'
+import useApp from './hooks/useApp'
+import PrivacyUnAuth from './components/privacy_block/PrivacyUnAuth'
+import FaqUnAuth from './components/faq_block/FaqUnAuth'
+import { observer } from 'mobx-react-lite'
+import Loader from './components/loaders/Loader'
+import { themeManager } from './managers/themeManager'
+import useThemeDetector from './hooks/useThemeDetector'
 
 const App = observer(() => {
+	const { user, courseData, localConfig } = useContext(Context)
+	const location = useLocation()
 
-    const {user, courseData, localConfig} = useContext(Context)
-    const location = useLocation();
+	const { isThemeDetector } = useThemeDetector()
 
-    const {isThemeDetector} = useThemeDetector()
+	const { isLoading, isAuth, firstAuth } = useApp()
+	if (isLoading) return <></>
 
+	return (
+		<div className='App'>
+			<ScrollToTop />
 
-    const {isLoading, isAuth, firstAuth} = useApp()
-    if (isLoading) return <></>
+			{localConfig.sky.value && (
+				<Sky isDark={themeManager(localConfig).isDark()} />
+			)}
 
-    return (
-        <div className="App">
+			<div className='main_container'>
+				<Header isAuth={isAuth} />
+				<main className='content_container'>
+					{isAuth && <Courses />}
 
-            <ScrollToTop/>
+					<Routes>
+						{/*LOGIN*/}
+						{!isAuth ? (
+							<Route path='/login' element={<Enter firstAuth={firstAuth} />} />
+						) : (
+							<Route path='/login' element={<Navigate to='/' replace />} />
+						)}
 
-            {localConfig.sky.value && <Sky isDark={themeManager(localConfig).isDark()}/>}
+						<Route path='/privacy' element={<PrivacyUnAuth />} />
 
-            <div className="main_container">
+						{/*FAQ*/}
+						{!isAuth ? (
+							<Route path='/faq' element={<FaqUnAuth />} />
+						) : (
+							<Route path='/faq' element={<FaqLogged />} />
+						)}
 
-                <Header isAuth={isAuth}/>
-                <main className="content_container">
+						{/*PRIVACY*/}
+						{!isAuth ? (
+							<Route path='/privacy' element={<PrivacyLogged />} />
+						) : (
+							<Route path='/privacy' element={<PrivacyUnAuth />} />
+						)}
 
-                    {isAuth && <Courses/>}
+						{/*<Route path="/faq" element={<FaqUnAuth/>}/>*/}
+						{/*<Route path="/faq_logged" element={<FaqLogged/>}/>*/}
 
-                    <Routes>
+						{/* auth */}
+						<Route path='/' element={<Schedule />} />
+						<Route path='/menu' element={<Menu />} />
+						<Route path='/course' element={<Course />} />
+						<Route path='/courses' element={<CoursesTablet />} />
+						<Route path='/deadlines' element={<DeadlineTablet />} />
+						<Route path='/task' element={<Task />} />
+						<Route path='/settings' element={<Settings />} />
+						{/*<Route path="/privacy_logged" element={<PrivacyLogged/>}/>*/}
+						{/*<Route path="/faq_logged" element={<FaqLogged/>}/>*/}
 
-                        {/*LOGIN*/}
-                        {!isAuth
-                            ? <Route path="/login" element={<Enter firstAuth={firstAuth}/>}/>
-                            : <Route path="/login" element={<Navigate to="/" replace/>}/>
-                        }
+						{/*Маршруты не авторизованного польховатля*/}
+					</Routes>
 
-
-                        <Route path="/privacy" element={<PrivacyUnAuth/>}/>
-
-                        {/*FAQ*/}
-                        {!isAuth
-                            ? <Route path="/faq" element={<FaqUnAuth/>}/>
-                            : <Route path="/faq" element={<FaqLogged/>}/>
-                        }
-
-                        {/*PRIVACY*/}
-                        {!isAuth
-                            ? <Route path="/privacy" element={<PrivacyLogged/>}/>
-                            : <Route path="/privacy" element={<PrivacyUnAuth/>}/>
-                        }
-
-                        {/*<Route path="/faq" element={<FaqUnAuth/>}/>*/}
-                        {/*<Route path="/faq_logged" element={<FaqLogged/>}/>*/}
-
-                        {/* auth */}
-                        <Route path="/" element={<Schedule/>}/>
-                        <Route path="/menu" element={<Menu/>}/>
-                        <Route path="/course" element={<Course/>}/>
-                        <Route path="/courses" element={<CoursesTablet/>}/>
-                        <Route path="/deadlines" element={<DeadlineTablet/>}/>
-                        <Route path="/task" element={<Task/>}/>
-                        <Route path="/settings" element={<Settings/>}/>
-                        {/*<Route path="/privacy_logged" element={<PrivacyLogged/>}/>*/}
-                        {/*<Route path="/faq_logged" element={<FaqLogged/>}/>*/}
-
-                        {/*Маршруты не авторизованного польховатля*/}
-
-
-                    </Routes>
-
-                    {isAuth && <Deadline/>}
-
-                </main>
-            </div>
-        </div>
-    );
+					{isAuth && <Deadline />}
+				</main>
+			</div>
+		</div>
+	)
 })
 
-export default App;
+export default App
--- src/components/menu_block/MenuThemeBlock.js
+++ src/components/menu_block/MenuThemeBlock.js
@@ -1,57 +1,88 @@
-import React, {useContext} from 'react';
-import {Moon, StarAndCrescent, Sun} from "@phosphor-icons/react";
-import {Context} from "../../index";
-import {observer} from "mobx-react-lite";
-import {themeManager} from "../../managers/themeManager";
+import React, { useContext } from 'react'
+import { Moon, StarAndCrescent, Sun } from '@phosphor-icons/react'
+import { Context } from '../../index'
+import { observer } from 'mobx-react-lite'
+import { themeManager } from '../../managers/themeManager'
 
 const MenuThemeBlock = observer(() => {
+	const { localConfig } = useContext(Context)
 
-    const {localConfig} = useContext(Context)
+	const setTheme = newTheme => {
+		themeManager(localConfig).setStore(newTheme)
+		themeManager(localConfig).setStyle()
 
-    const setTheme = newTheme => {
-        themeManager(localConfig).setStore(newTheme)
-        themeManager(localConfig).setStyle()
-    }
+		// Получение значения из localStorage
+		const theme = localStorage.getItem('theme')
 
-    const invBg = theme =>
-        localConfig.theme === theme ? 'enabled_invert' : ''
+		// Проверка значения и изменение theme-color
+		if (theme === 'light') {
+			document
+				.querySelector('meta[name="theme-color"]')
+				.setAttribute('content', '#ededed')
+		} else if (theme === 'dark') {
+			document
+				.querySelector('meta[name="theme-color"]')
+				.setAttribute('content', 'black')
+		} else if (theme === 'system') {
+			// Если значение 'system', изменить на основе темы устройства
+			const mediaQuery = window.matchMedia('(prefers-color-scheme: light)')
+			if (mediaQuery.matches) {
+				document
+					.querySelector('meta[name="theme-color"]')
+					.setAttribute('content', '#ededed')
+			} else {
+				document
+					.querySelector('meta[name="theme-color"]')
+					.setAttribute('content', 'black')
+			}
+		}
+	}
 
-    const invTxt = theme =>
-        localConfig.theme === theme ? 'theme_enabled_text' : ''
+	const invBg = theme => (localConfig.theme === theme ? 'enabled_invert' : '')
 
-    return (
-        <div className="content_cover">
-            <div className="content_elem_column">
-                <p>Оформление</p>
-                <div className="container_row_start">
+	const invTxt = theme =>
+		localConfig.theme === theme ? 'theme_enabled_text' : ''
 
-                    {/*  LIGHT  */}
-                    <div className="theme_item_container" onClick={() => setTheme('light')}>
-                        <div className={`theme_circle ${invBg('light')}`}>
-                            <Sun weight="bold" className="icon_big"/>
-                        </div>
-                        <p className={`theme_text ${invTxt('light')}`}>Светлое</p>
-                    </div>
-                    {/*  DARK  */}
-                    <div className="theme_item_container" onClick={() => setTheme('dark')}>
-                        <div className={`theme_circle ${invBg('dark')}`}>
-                            <Moon weight="bold" className="icon_big"/>
-                        </div>
-                        <p className={`theme_text ${invTxt('dark')}`}>Темное</p>
-                    </div>
+	return (
+		<div className='content_cover'>
+			<div className='content_elem_column'>
+				<p>Оформление</p>
+				<div className='container_row_start'>
+					{/*  LIGHT  */}
+					<div
+						className='theme_item_container'
+						onClick={() => setTheme('light')}
+					>
+						<div className={`theme_circle ${invBg('light')}`}>
+							<Sun weight='bold' className='icon_big' />
+						</div>
+						<p className={`theme_text ${invTxt('light')}`}>Светлое</p>
+					</div>
+					{/*  DARK  */}
+					<div
+						className='theme_item_container'
+						onClick={() => setTheme('dark')}
+					>
+						<div className={`theme_circle ${invBg('dark')}`}>
+							<Moon weight='bold' className='icon_big' />
+						</div>
+						<p className={`theme_text ${invTxt('dark')}`}>Темное</p>
+					</div>
 
-                    {/*  SYSTEM  */}
-                    <div className="theme_item_container" onClick={() => setTheme('system')}>
-                        <div className={`theme_circle ${invBg('system')}`}>
-                            <StarAndCrescent weight="bold" className="icon_big"/>
-                        </div>
-                        <p className={`theme_text ${invTxt('system')}`}>Системное</p>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    );
+					{/*  SYSTEM  */}
+					<div
+						className='theme_item_container'
+						onClick={() => setTheme('system')}
+					>
+						<div className={`theme_circle ${invBg('system')}`}>
+							<StarAndCrescent weight='bold' className='icon_big' />
+						</div>
+						<p className={`theme_text ${invTxt('system')}`}>Системное</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 })
 
-export default MenuThemeBlock;
+export default MenuThemeBlock
