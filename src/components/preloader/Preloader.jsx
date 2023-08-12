644c59d5f7f7b3482d177603e91409e01b764a3d
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-08-12 20:32:38 +0300

    preloader animation

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -1,15 +1,13 @@
-
 import logo from './space_logo.svg'
+import { CircleNotch } from '@phosphor-icons/react'
 
 const Preloader = () => {
 	return (
-			<div className='preloader'>
-				<img alt='' src={logo} className='preloader_logo' />
-				{/* <img
-									alt=""
-									src={logo}
-									className='preloader_logo_back'/> */}
-			</div>
+		<div className='preloader'>
+			<img alt='' src={logo} className='preloader_logo' />
+			<img alt='' src={logo} className='preloader_logo_back' />
+			{/* <CircleNotch weight="bold" className="icon_min preloader-notch" /> */}
+		</div>
 	)
 }
 
