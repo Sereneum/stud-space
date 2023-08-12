808762f9af3deeca7d5febd749969bd3100b2697
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-08-12 11:46:20 +0300

    manifest changes

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import Loader from './components/loaders/Loader'
 import { themeManager } from './managers/themeManager'
 import useThemeDetector from './hooks/useThemeDetector'
 import Preloader from './components/preloader/Preloader'
+import { CSSTransition } from 'react-transition-group'
 
 const App = observer(() => {
 	const { user, courseData, localConfig } = useContext(Context)
@@ -38,33 +39,43 @@ const App = observer(() => {
 
 	const { isLoading, isAuth, firstAuth } = useApp()
 
-	if (isLoading) return <>   <Preloader />    </>
+	if (isLoading)
+		return (
+			<CSSTransition
+				in={isLoading}
+				timeout={300}
+				classNames='preloader'
+				unmountOnExit
+			>
+				<Preloader />
+			</CSSTransition>
+		)
 
-    	// Получение значения из localStorage
-		const theme = localStorage.getItem('theme')
+	// Получение значения из localStorage
+	const theme = localStorage.getItem('theme')
 
-		// Проверка значения и изменение theme-color
-		if (theme === 'light') {
+	// Проверка значения и изменение theme-color
+	if (theme === 'light') {
+		document
+			.querySelector('meta[name="theme-color"]')
+			.setAttribute('content', '#ededed')
+	} else if (theme === 'dark') {
+		document
+			.querySelector('meta[name="theme-color"]')
+			.setAttribute('content', 'black')
+	} else if (theme === 'system') {
+		// Если значение 'system', изменить на основе темы устройства
+		const mediaQuery = window.matchMedia('(prefers-color-scheme: light)')
+		if (mediaQuery.matches) {
 			document
 				.querySelector('meta[name="theme-color"]')
 				.setAttribute('content', '#ededed')
-		} else if (theme === 'dark') {
+		} else {
 			document
 				.querySelector('meta[name="theme-color"]')
 				.setAttribute('content', 'black')
-		} else if (theme === 'system') {
-			// Если значение 'system', изменить на основе темы устройства
-			const mediaQuery = window.matchMedia('(prefers-color-scheme: light)')
-			if (mediaQuery.matches) {
-				document
-					.querySelector('meta[name="theme-color"]')
-					.setAttribute('content', '#ededed')
-			} else {
-				document
-					.querySelector('meta[name="theme-color"]')
-					.setAttribute('content', 'black')
-			}
 		}
+	}
 
 	return (
 		<div className='App'>
--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -1,18 +1,16 @@
+
 import logo from './space_logo.svg'
 
 const Preloader = () => {
 	return (
-			<div className="preloader">
-							 <img
-									alt=""
-									src={logo}
-									className='preloader_logo'/>
-							{/* <img
+			<div className='preloader'>
+				<img alt='' src={logo} className='preloader_logo' />
+				{/* <img
 									alt=""
 									src={logo}
 									className='preloader_logo_back'/> */}
 			</div>
-	);
-};
+	)
+}
 
-export default Preloader;
+export default Preloader
