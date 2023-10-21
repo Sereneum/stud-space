db5c4f4b0e3738c9cdf7c867ab2f1d3a33902a2f
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-10-21 13:31:25 +0300

    feat: add information block structure in Course.js

--- src/managers/statusManager.js
+++ src/managers/statusManager.js
@@ -11,31 +11,31 @@ export const getStatusIcon = (statusID) => {
   if (statusID === 0)
     return {
       color: 'not_checked',
-      icon: <WarningCircle weight="bold" className="icon_min" />,
+      icon: <WarningCircle weight="bold" className="icon_min icon-lighter" />,
       text: '',
     };
   if (statusID === 1)
     return {
       color: 'declined',
-      icon: <X weight="bold" className="icon_min text_lighter" />,
+      icon: <X weight="bold" className="icon_min icon-lighter text_lighter" />,
       text: 'text_lighter',
     };
   if (statusID === 2)
     return {
       color: 'exam',
-      icon: <Clock weight="bold" className="icon_min text_lighter" />,
+      icon: <Clock weight="bold" className="icon_min icon-lighter text_lighter" />,
       text: 'text_lighter',
     };
   if (statusID === 3)
     return {
       color: 'pract',
-      icon: <PencilSimple weight="bold" className="icon_min text_lighter" />,
+      icon: <PencilSimple weight="bold" className="icon_min icon-lighter text_lighter" />,
       text: 'text_lighter',
     };
   if (statusID === 4)
     return {
       color: 'lect',
-      icon: <Check weight="bold" className="icon_min text_lighter" />,
+      icon: <Check weight="bold" className="icon_min icon-lighter text_lighter" />,
       text: 'text_lighter',
     };
 };
